d8e5dc110840de3b258ea622822da87689732ec4
Alex Robinson <github@alex.fu2k.org>
2018-06-21 22:20:15 +0100

    Integrate runtime methods and expose via single getRuntimeData method

--- .wallaby.conf.js
+++ .wallaby.conf.js
@@ -1,10 +1,13 @@
 module.exports = () => ({
   files: [
+    { pattern: 'index.js', load: false },
+    "!index.unit.spec.js",
     { pattern: 'lib/**/*.js', load: false },
     "!lib/**/*.unit.spec.js",
     { pattern: 'data/**/*.json', load: false }
   ],
   tests: [
+    'index.unit.spec.js',
     'lib/**/*.unit.spec.js'
   ],
   env: {
