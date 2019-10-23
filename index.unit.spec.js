61c0675042935e0b49fe88e9d7934d6017821abc
Jonathan Perry <jonathanperry@sequencemedia.net>
2019-10-23 14:26:56 +0100

    `.gitignore` - CircleCI - Dependabot

--- index.unit.spec.js
+++
@@ -1,9 +0,0 @@
-const test = require('tape')
-
-const methods = require('./index')
-
-test('When the module is loaded', function (t) {
-  t.equal(typeof methods.getRuntimeData, 'function', 'it should export the getRuntimeData method')
-
-  t.end()
-})
